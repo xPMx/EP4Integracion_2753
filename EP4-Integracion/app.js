8885972f55044c1d5ff43b9d9c6f401a4f5cce4e
PM <pieromolfino96@gmail.com>
2024-07-08 18:05:19 -0500

    MolfinoPiero
    EP4 Integracion NRC: 2753

---
+++ EP4-Integracion/app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const productosRouter = require("./routes/productos");
+const clientesRouter = require("./routes/clientes");
+const ordenesRouter = require("./routes/ordenes");
+const categoriasRouter = require("./routes/categorias");
+
+const sequelize = require("./config/database");
+
+sequelize
+  .sync()
+  .then(() => {
+    console.log("Base de datos sincronizada");
+    // app.listen(PORT, () => {
+    //   console.log(`Servidor corriendo en puerto ${PORT}`);
+    // });
+  })
+  .catch((error) => {
+    console.error("Error sincronizando la base de datos:", error);
+  });
+
+app.use(express.json());
+app.use("/api/productos", productosRouter);
+app.use("/api/clientes", clientesRouter);
+app.use("/api/ordenes", ordenesRouter);
+app.use("/api/categorias", categoriasRouter);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
+});
